2223f44756217ae1aeb394434b97b6365ef04644
Jeongyeon Park <ldsjypark@gmail.com>
2026-02-13 01:33:12 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import { useState } from 'react';
-import { useSingleSound } from './SingleSoundContext';
+import { useState, useRef, useEffect } from 'react';
+import useSound from 'use-sound';
 
 const phrases = [
   "No",
   "Nah I'm good",
   "Are you really sure?",
   "Pretty please?",
-  "Pretty pretty please?",
   "You're so mean 😢",
   "I'm going to be sad 😭",
   "Yes is the only answer",
@@ -16,48 +15,86 @@ const phrases = [
 const CATS = {
   happy: {
     id: 'happy',
-    gif: '/images/happy.gif', //downloaded
-    mp3: '/sounds/happy.mp3', //downloaded
+    gif: '/images/happy.gif',
+    mp3: '/sounds/happy.mp3',
   },
   dancing: {
     id: 'dancing',
-    gif: '/images/dancing.gif', //downloaded
-    mp3: '/sounds/dancing.mp3', //TODO: chinese tiktok dance song
+    mp4: '/videos/dancing.mp4',
   },
   huh: {
     id: 'huh',
-    gif: '/images/huh.gif', //downloaded
-    mp3: '/sounds/huh.mp3', //downloaded
+    gif: '/images/huh.gif',
+    mp3: '/sounds/huh.mp3',
   },
   headache: {
     id: 'headache',
-    gif: '/images/headache.png', //downloaded
-    mp3: '/sounds/headache.mp3', //TODO: headache sound
+    mp4: '/videos/headache.mp4',
   },
-  oiia: {
-    id: 'oiia',
-    gif: '/images/oiia.gif', //downloaded
-    mp3: '/sounds/oiia.mp3', //downloaded
+  oiiaStanding: {
+    id: 'oiiaStanding',
+    gif: '/images/oiia-standing.gif',
+  },
+  oiiaSpinning: {
+    id: 'oiiaSpinning',
+    gif: '/images/oiia-spinning.gif',
+    mp3: '/sounds/oiia.mp3',
   },
   sad: {
     id: 'sad',
-    gif: '/images/sad.gif', //downloaded
-    mp3: '/sounds/sad.mp3', //downloaded
+    gif: '/images/sad.gif',
+    mp3: '/sounds/sad.mp3',
   },
   suspicious: {
     id: 'suspicious',
-    gif: '/images/suspicious.gif', //downloaded
-    mp3: '/sounds/suspicious.mp3', //downloaded
+    gif: '/images/suspicious.gif',
+    mp3: '/sounds/suspicious.mp3',
   },
 };
 
 function App() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
-  const [playHappyCatSound] = useSingleSound(CATS.happy.id, CATS.happy.mp3);
+  const [isDancingMuted, setIsDancingMuted] = useState(true);
+  const happyGifRef = useRef(null);
+  const dancingVideoRef = useRef(null);
+  const [playHappyCatSound, { stop: stopHappyCatSound }] = useSound(CATS.happy.mp3, { loop: true, interrupt: true });
+  const [playHuhSound, { stop: stopHuhSound }] = useSound(CATS.huh.mp3);
+  const [playSuspiciousSound, { stop: stopSuspiciousSound }] = useSound(CATS.suspicious.mp3);
+  const [playSadSound, { stop: stopSadSound }] = useSound(CATS.sad.mp3);
+
+  useEffect(() => {
+    if (yesPressed) playHappyCatSound();
+  }, [yesPressed, playHappyCatSound]);
+
+  function handleHappyGifClick() {
+    if (!isDancingMuted) {
+      setIsDancingMuted(true);
+      playHappyCatSound();
+    }
+  }
+
+  function handleDancingVideoClick() {
+    if (isDancingMuted) {
+      setIsDancingMuted(false);
+      stopHappyCatSound();
+    }
+  }
 
   function handleNoClick() {
     if (noCount < phrases.length - 1) {
+      if (noCount === 0) {
+        setTimeout(playHuhSound, 1000);
+      } else if (noCount === 1) {
+        stopHuhSound();
+        playSuspiciousSound();
+      } else if (noCount === 2) {
+        stopSuspiciousSound();
+        playSadSound();
+      } else if (noCount === 3) {
+        stopSadSound();
+      }
+
       setNoCount(noCount + 1);
     }
   }
@@ -70,18 +107,60 @@ function App() {
     <div className="container">
       {yesPressed ? (
         <>
-          <img
-            src={CATS.happy.gif}
-            onClick={playHappyCatSound}
-          />
+          <div className="mediaRow">
+            <video
+              ref={dancingVideoRef}
+              src={CATS.dancing.mp4}
+              loop
+              autoPlay
+              playsInline
+              muted={isDancingMuted}
+              onClick={handleDancingVideoClick}
+              className="mediaItem"
+            />
+            <img
+              ref={(happyGifRef)}
+              src={CATS.happy.gif}
+              onClick={handleHappyGifClick}
+              className="mediaItem"
+            />
+          </div>
           <h2>Yay! Lets go get some food now 🥘</h2>
         </>
       ) : (
         <>
-          <img
-            className='jumpingBearImg'
-            src="/images/bear-roses.gif"
-          />
+          {noCount === 5 ? (
+            <></>
+          ) : noCount === 4 ? (
+            <video
+              src={CATS.headache.mp4}
+              loop
+              autoPlay
+              playsInline
+              className="noImg"
+              style={{ cursor: 'pointer' }}
+            />
+          ) : noCount === 3 ? (
+            <img
+              className='noImg'
+              src={CATS.sad.gif}
+            />
+          ) : noCount === 2 ? (
+            <img
+              className='noImg'
+              src={CATS.suspicious.gif}
+            />
+          ) : noCount === 1 ? (
+            <img
+              className='noImg'
+              src={CATS.huh.gif}
+            />
+          ) : (
+            <img
+              className='noImg'
+              src={'/images/bear-roses.gif'}
+            />
+          )}
           <h2>Will you be my valentine Soeun?</h2>
           <div className='buttonContainer'>
             <button
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { SingleSoundProvider } from './SingleSoundContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SingleSoundProvider>
       <App />
-    </SingleSoundProvider>
   </React.StrictMode>
 );
